f4db3829483069b9a3c7534c237abb8e639a5591
Karl <karl_dilma@hotmail.com>
2024-05-09 16:45:32 +1000

    feat: grabbing foodData

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react";
+
+import { Food, foodData } from "./data";
 import "./App.css";
 
 const App = (): JSX.Element => {
+  const [food, setFood] = useState<Food[]>([]);
+
+  useEffect(() => {
+    setFood(foodData);
+  }, []);
   return <>SweetShop</>;
 };
 
